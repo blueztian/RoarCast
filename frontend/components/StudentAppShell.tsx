69a0f81b0ad8b54999fb084ea3887d4891414148
blueztian <chris.balinado20@gmail.com>
2026-07-24 14:36:42 +0800

    refactor(student): abolish desktop views and landing pages for mobile-first focus

--- frontend/components/StudentAppShell.tsx
+++ frontend/components/StudentAppShell.tsx
@@ -1,11 +1,9 @@
-import DesktopTopNav from "./DesktopTopNav";
 import MobileFloatingNav from "./MobileFloatingNav";
 
 export default function StudentAppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-[#fcfbf9]">
-      <DesktopTopNav />
-      <main className="flex-1 pb-24 md:pb-0 md:pt-[76px]">
+    <div className="flex min-h-[100svh] w-full flex-col bg-background">
+      <main className="flex-1 pb-24">
         {children}
       </main>
       <MobileFloatingNav />
